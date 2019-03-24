72dc9b89900889952ef3956ae65ae237afc3072e
ivalue2333 <1372254949@qq.com>
2019-03-24 16:26:08 +0800

    demo

--- es/axios_learn/static/app.js
+++ es/axios_learn/static/app.js
@@ -1,28 +1,35 @@
 /**
 
-*/
+ */
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  },
-  methods: {
-    get_user_id: function() {
-        axios.get('/users/12345').then(function(response){
-            this.message = response.data['data']
-            alert(this.message)
-        })
+    el: '#app',
+    data: function () {
+        return {
+            'message': '123'
+        }
+    },
+
+    created: function () {
+        this.get_user_id()
+    },
+
+    methods: {
+        get_user_id: function () {
+            axios.get('/users/12345').then(function (response) {
+                this.message = response.data['data']
+                alert(this.message)
+            })
+        }
     }
-  }
 })
 
 axios.get('/users/12345')
-  .then(function(response) {
-    console.log(response.data);
-    console.log(response.data['data']);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  });
+    .then(function (response) {
+        console.log(response.data);
+        console.log(response.data['data']);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+    });
